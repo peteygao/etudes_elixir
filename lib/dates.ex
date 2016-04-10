adc46f5a6ba20c8e085d0374e302a4d8cac21263
peteygao <me@petergao.com>
2016-04-10 02:45:43 -0700

    Etudes 6-2 Accumulating the Sum of a List

--- lib/dates.ex
+++ lib/dates.ex
@@ -15,4 +15,55 @@ defmodule Dates do
     String.split(string, "-")
       |> Enum.map(&(:erlang.binary_to_integer &1))
   end
+
+  @doc """
+    Etudes 6-2
+    Returns the date of the year (taking into account leap years).
+  """
+  @spec julian(String.t()) :: number()
+
+  def julian(string) do
+    [year, month, day] = date_parts(string)
+
+    month_total(month, year) + day
+  end
+
+  @doc """
+    Etudes 6-2
+    Returns the total number of dates in preceding months,
+    including leap days where applicable.
+    Requires the month and year as inputs.
+  """
+  @spec month_total(integer(), integer()) :: integer()
+
+  def month_total(month, year) do
+    date_list = cond do
+      is_leap_year(year) -> [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+      true               -> [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    end
+
+    month_total month, date_list, 0
+  end
+
+  defp month_total(months, date_list, acc) do
+    cond do
+      1 == months -> acc
+      true -> month_total months - 1, tl(date_list), hd(date_list) + acc
+    end
+  end
+
+  @doc """
+    Etudes 6-2
+    Returns true if the year is a leap year, which is calculated
+    according to this logic:
+    Year is divisible by 4 AND year is NOT divisible by 100
+    OR
+    Year is divisible by 400
+  """
+  @spec is_leap_year(integer()) :: boolean()
+
+  def is_leap_year(year) do
+    (rem(year, 4) == 0 and rem(year, 100) != 0)
+    or (rem(year, 400) == 0)
+  end
 end
--- test/dates_test.exs
+++ test/dates_test.exs
@@ -5,4 +5,42 @@ defmodule DatesTest do
   test "date_parts splits 2016-04-10" do
     assert Dates.date_parts("2016-04-10") == [2016, 4, 10]
   end
+
+  test "get the date of the year in a regular year" do
+    assert Dates.julian("2013-12-31") == 365
+  end
+
+  test "get the date of the year in a leap year" do
+    assert Dates.julian("2012-12-31") == 366
+  end
+
+  test "get the number of days in a leap year if it's after Feb 29" do
+    assert Dates.julian("2000-03-01") == 61
+  end
+
+  test "get the number of days in a leap year if it's before Feb 29" do
+    assert Dates.julian("2000-02-01") == 32
+  end
+
+  test "new years!" do
+    assert Dates.julian("2016-01-01") == 1
+  end
+
+  test "leap year is every 4 years, except if it's a new century." do
+    assert Dates.is_leap_year(1904)
+    refute Dates.is_leap_year(1900)
+  end
+
+  test "leap year is every 400 years, even if it's a new century." do
+    assert Dates.is_leap_year(2000)
+    assert Dates.is_leap_year(400)
+  end
+
+  test "get the number of days in preceding months" do
+    assert Dates.month_total(4, 2013) == 90
+  end
+
+  test "get the number of days in preceding months in a leap year" do
+    assert Dates.month_total(4, 2012) == 91
+  end
 end
