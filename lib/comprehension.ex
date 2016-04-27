1df1f66d38b480cc77d021e9156fd84310ce46f8
peteygao <me@petergao.com>
2016-04-27 03:53:33 -0400

    Add typespecs

--- lib/comprehension.ex
+++ lib/comprehension.ex
@@ -6,7 +6,7 @@ defmodule Comprehension do
   @doc """
   Return a list of all male that are over age 40
   """
-  @spec male_over_40(list) :: list(tuple())
+  @spec male_over_40(list) :: list(tuple)
 
   def male_over_40 people do
     for {name, gender, age} <- people,
@@ -18,6 +18,8 @@ defmodule Comprehension do
   @doc """
   Returns a list of people that are either male _or_ are over 40
   """
+  @spec male_or_over_40(list) :: list(tuple)
+
   def male_or_over_40 people do
     for {name, gender, age} <- people,
         is_male_or_over_40?(gender, age),
